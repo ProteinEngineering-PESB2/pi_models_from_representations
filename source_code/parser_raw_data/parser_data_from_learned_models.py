64441d4f79362c108126e7e7ee849f6f1ea3dbdc
dMedinaO <41024522+dMedinaO@users.noreply.github.com>
2022-10-05 18:11:21 -0800

    adding parser for raw data

--- source_code/parser_raw_data/parser_data_from_learned_models.py
+++ source_code/parser_raw_data/parser_data_from_learned_models.py
@@ -8,10 +8,22 @@ path_export = sys.argv[2]
 column_seq = sys.argv[3]
 column_response = sys.argv[4]
 column_id = sys.argv[5]
+name_path_to_create = sys.argv[6]
 
 #create a data frame
+print("Reading dataframe")
 df_export = pd.DataFrame()
 
-#create 
+#create dataframe
+print("Creating dataframe")
 for column in [column_id, column_seq, column_response]:
     df_export[column] = df_data[column]
+
+#create dir
+command = "mkdir {}{}".format(path_export, name_path_to_create)
+print(command)
+os.system(command)
+
+#export dataframe
+print("Export dataframe")
+df_export.to_csv("{}{}\\parsed_dataframe.csv".format(path_export, name_path_to_create), index=False)
